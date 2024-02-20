72b4fb14b169ff08cc6044160f4968025d796596
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-20 19:37:44 +0100

    add conditions inside function and add div total_price in html that show the final price

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,32 @@ const input_km = document.getElementById('user_km'); // input for km
 
 const input_age = document.getElementById('user_age'); // input for age
 
-// connect button to send data to console
+
+
+// connect button to send data to console / document page
 
 document.getElementById('btn').addEventListener('click',  // button to send data
-function(){
-    console.log(input_age.value, input_km.value);
-})
+    function () {
+
+        const kmNumber = Number(input_km.value);
+
+        const ageNumber = Number(input_age.value);
+
+        let total_price = kmNumber * 0.21; // standard price
+
+        if (ageNumber < 18) {
+
+            total_price = total_price / 100 * 80; // 20% discounted price
+
+
+        } else if (ageNumber > 65) {
+
+            total_price = total_price / 100 * 60; // 40% discounted price
+
+        }
+
+        // console.log(kmNumber, ageNumber);
+        // console.log('Il prezzo del tuo biglietto è: ' + total_price.toFixed(2) + ' \u20AC' );
+
+        let totalPrice = document.getElementById('total_price').innerHTML = ( total_price.toFixed(2) + ' \u20AC' );
+    })
