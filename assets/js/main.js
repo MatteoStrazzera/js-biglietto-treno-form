1317713f4aa718b65ebb1049f6d4788a7a7fcb82
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-20 17:30:57 +0100

    add variables to connect to inputs and connect button to send data to console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('collegato');
+// console.log('collegato');
+
+// connect variables to inputs
+
+const input_km = document.getElementById('user_km'); // input for km
+
+const input_age = document.getElementById('user_age'); // input for age
+
+// connect button to send data to console
+
+document.getElementById('btn').addEventListener('click',  // button to send data
+function(){
+    console.log(input_age.value, input_km.value);
+})
